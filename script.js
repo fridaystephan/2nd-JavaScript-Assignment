58491f21c07ccf9a424c387c5727aa924995348d
stephen <fridaystephan@gmail.com>
2023-01-30 18:20:28 +0100

    Third commit. Added Math.floor

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function lifeInWeeks() {
   const age = document.getElementById("current-age").value;
   const daysLeft = (90 - age) * 365;
-  const weeksLeft = daysLeft / 7;
-  const monthsLeft = daysLeft / 30;
+  const weeksLeft = Math.floor(daysLeft / 7);
+  const monthsLeft = Math.floor(daysLeft / 30);
   document.getElementById("output").innerHTML = `You have ${daysLeft} days, ${weeksLeft} weeks, and ${monthsLeft} months left.`;
 }
 
